46c305a8aa450c06d93ca619046b05fc3f757b14
Adwaith jayan <aj007boss@gmail.com>
2025-01-01 22:07:16 +0530

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off", // Disable the rule here
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars":"off",
+      "@typescript-eslint/no-non-null-asserted-optional-chain":"off",
+      "@typescript-eslint/no-unused-expressions":"off",
     },
   },
 ];
